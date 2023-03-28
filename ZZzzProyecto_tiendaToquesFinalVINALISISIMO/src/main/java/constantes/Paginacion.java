0fa4c5ae23fea404a8d527d3753a305cfb908d9f
Miki Pardo <92552184+mikipardo@users.noreply.github.com>
2023-03-29 01:26:09 +0200

    c

--- ZZzzProyecto_tiendaToquesFinalVINALISISIMO/src/main/java/constantes/Paginacion.java
+++ ZZzzProyecto_tiendaToquesFinalVINALISISIMO/src/main/java/constantes/Paginacion.java
@@ -4,5 +4,6 @@ public class Paginacion {
 
 	public static int RESULTADOS_POR_PAGINA = 12;
 	public static int RESULTADOS_POR_PAGINA_ADMIN = 6;
-	public static int RESULTADOS_POR_PAGINA_ADMIN_USER = 8;
+	public static int RESULTADOS_POR_PAGINA_ADMIN_USER = 8; 
+	
 }
